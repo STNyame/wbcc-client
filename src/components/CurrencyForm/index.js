a14b52bfbf4d98037e2393fa5ad98a72a13dce43
STNyame <sentwalidors@hotmail.com>
2021-06-15 11:52:22 +0200

    add form select for currency 1 & 2 and map options

--- src/components/CurrencyForm/index.js
+++ src/components/CurrencyForm/index.js
@@ -1,6 +1,25 @@
 import { Col, Form, Button } from "react-bootstrap";
+import { useEffect, useState } from "react";
+const currencyData = require("../../api.json");
 
 export default function CurrencyForm() {
+  const [data, setData] = useState();
+  const [currencyOne, setCurrencyOne] = useState("EUR");
+  const [currencyTwo, setCurrencyTwo] = useState("USD");
+  useEffect(() => {
+    // const fetchData = async () => {
+    //   try {
+    //     const res = await axios.get(
+    //       `http://api.exchangeratesapi.io/v1/latest?access_key=627eac13bdbc305e6e615008b83e46c5&format=1`
+    //     );
+    //     setData(res.data);
+    //   } catch (e) {
+    //     console.log(e.message);
+    //   }
+    // };
+    // fetchData();
+    setData(currencyData);
+  }, []);
   return (
     <div>
       <Form>
@@ -9,19 +28,38 @@ export default function CurrencyForm() {
             <Form.Control placeholder="Amount" />
           </Col>
           <Col>
-            <Form.Control as="select">
-              <option>Currency 1</option>
+            <Form.Control
+              as="select"
+              value={currencyOne}
+              onChange={(e) => setCurrencyOne(e.target.value)}
+            >
+              {data &&
+                Object.keys({ ...data.rates }).map((item, i) => (
+                  <option value={item} key={i}>
+                    {item}
+                  </option>
+                ))}
             </Form.Control>
           </Col>
           <Col>
             <Button variant="primary">Bidirectional</Button>
           </Col>
           <Col>
-            <Form.Control as="select">
-              <option>Currency 2</option>
+            <Form.Control
+              as="select"
+              value={currencyTwo}
+              onChange={(e) => setCurrencyTwo(e.target.value)}
+            >
+              {data &&
+                Object.keys({ ...data.rates }).map((item, i) => (
+                  <option value={item} key={i}>
+                    {item}
+                  </option>
+                ))}
             </Form.Control>
           </Col>
         </Form.Row>
+        <Button variant="primary">Convert</Button>
       </Form>
     </div>
   );
