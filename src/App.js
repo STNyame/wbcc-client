0f4572cbb3987ee62eb0f5efce73a3bb70bb14d8
STNyame <sentwalidors@hotmail.com>
2021-06-15 17:33:06 +0200

    add rate(2020)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ const currencyData = require("./api.json");
 const chartData = require("./apiChartData.json");
 
 function App() {
+  // useEffect(() => {
+  // const fetchData = async () => {
+  //   try {
+  //     const res = await axios.get(
+  //       `http://api.exchangeratesapi.io/v1/latest?access_key=627eac13bdbc305e6e615008b83e46c5&format=1`
+  //     );
+  //     setData(res.data);
+  //   } catch (e) {
+  //     console.log(e.message);
+  //   }
+  // };
+  // fetchData();
+  // }, []);
   return (
     <div className="App">
       <header className="App-header">
